8928f6644d28803060f3b0b11ca846160652af3c
mehedee92 <49508551+mehedee92@users.noreply.github.com>
2019-09-25 20:46:45 +0600

    Update index.js

--- index.js
+++ index.js
@@ -30,31 +30,31 @@ function makeSound(key) {
 
   switch (key) {
     case "w":
-         var tom1 = new Audio("sound/tom-1.wav");
+         var tom1 = new Audio("tom-1.wav");
          tom1.play();
       break;
     case "a":
-         var tom2 = new Audio("sound/tom-2.wav");
+         var tom2 = new Audio("tom-2.wav");
          tom2.play();
       break;
     case "s":
-          var tom3 = new Audio("sound/tom-3.wav");
+          var tom3 = new Audio("tom-3.wav");
           tom3.play();
       break;
     case "d":
-          var tom4 = new Audio("sound/tom-4.wav");
+          var tom4 = new Audio("tom-4.wav");
           tom4.play();
       break;
     case "j":
-          var snare = new Audio("sound/snare.wav");
+          var snare = new Audio("snare.wav");
           snare.play();
       break;
     case "k":
-          var crash = new Audio("sound/crash.wav");
+          var crash = new Audio("crash.wav");
           crash.play();
       break;
     case "l":
-          var kick = new Audio("sound/kick.wav");
+          var kick = new Audio("kick.wav");
           kick.play();
       break;
 
